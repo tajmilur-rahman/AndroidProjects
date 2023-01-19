5fd344d26b530c0bff1b3e31be5b2a2993c5331b
Tajmilur Rahman <rupak.karmadhar@gmail.com>
2023-01-19 15:45:26 -0500

    Minor changes

---
+++ ButtonClick/app/src/main/java/com/example/buttonclick/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.buttonclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonA;
+    Button buttonB;
+    Button buttonC;
+    TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonA = findViewById(R.id.buttonA);
+        buttonB = findViewById(R.id.buttonB);
+        buttonC = findViewById(R.id.buttonC);
+        txt1 = findViewById(R.id.txt1);
+
+        buttonA.setOnClickListener(this);
+        buttonB.setOnClickListener(this);
+        buttonC.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.buttonA) {
+            txt1.setText("A Clicked");
+        }
+        if(v.getId() == R.id.buttonB) {
+            txt1.setText("B Clicked");
+        }
+        if(v.getId() == R.id.buttonC) {
+            txt1.setText("C Clicked");
+        }
+    }
+}
